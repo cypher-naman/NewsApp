af9a558fe8e1822c80943286778f1f47aa3dc211
Naman Sharma <namansharma@Namans-MacBook-Air.local>
2019-12-22 11:03:34 +0530

    complete project

---
+++ H4X0R News/Views/ContentView.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView {
+            List(networkManager.posts) { post in
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack {
+                        Text(String("Hits :\n \(post.points)"))
+                        Text(post.title)
+                    }
+                }
+            }
+            .navigationBarTitle("SwiftUI News App")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ Readme.md
@@ -0,0 +1,10 @@
+#  NEWS APP
+
+![](NewsApp.gif)
+
+## About:-
+
+This App uses the latest SwiftUI to design screens of the app which are much more faster and efficient in creating the UI. The app also calls to hacker news API to fetch top stories and display them in their order of upvotes. SwiftUI also enables the app to provide support to dark mode and the new screens opened utilizes web views to navigate to respective story URL.
+
+
+> courtesy appbrewery for the course and skeleton projects.
